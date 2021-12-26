fcbcde84d2ba76dbd66fa426981c7ef8132e50dd
xivilay <xivilay@gmail.com>
2021-12-26 17:44:59 +0100

    moved all precompiled from jniLibs to cmakelists

--- src/GuiConstants.h
+++ src/GuiConstants.h
@@ -1,8 +1,8 @@
 #pragma once
 
 struct GuiConstants {
-    inline static const int minWidth = 500;
+    inline static const int minWidth = 200;
     inline static const int maxWidth = 1900;
-    inline static const int minHeight = 300;
+    inline static const int minHeight = 200;
     inline static const int maxHeight = 1000;
 };
